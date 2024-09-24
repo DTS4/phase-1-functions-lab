0a2572539c5cd66d7b38f1b0e1a7351a97c6a0cf
Keith <keith.githinji@student.moringaschool.com>
2024-09-24 14:37:13 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution in this file!
+
+// function distanceFromHqInBlocks() {
+//     if (distanceFromHqInBlocks = 43) {
+//     } return 1 
+
+// }
+
+// function distanceFromHqInFeet() {
+//     if (distanceFromHqInFeet = 50) {
+//     } return 8
+
+// }
+
+// function distanceFromHqInBlocks(someValue){
+//   return distanceFromHqInBlocks()
+
+// }
+
+// distanceFromHqInBlocks(43)
+
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42
+    return Math.abs(someValue - hqLocation);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264
+    }
+
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination); {
+        if (distance <= 400) {
+            return 0
+        }
+    }
+    if (distance >= 400 && distance <= 2000) {
+        return (distance - 400) * 0.02
+    }else if (distance >= 2000 && distance <=2500) {
+        return 25
+    } else if (distance > 2500) {
+        return `cannot travel that far`
+    }
+
+}
+
+ 
